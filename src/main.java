06d55389cdf247dd730b0be6cc2b2cd55218d2b9
Churikov-SV <Churikov.SV@outlook.com>
2024-09-25 22:49:07 +0300

    Add main files

---
+++ src/main.java
@@ -0,0 +1,8 @@
+import controller.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        controller controller = new controller();
+        controller.start();
+    }
+}
