86677c098931b74e4fd8b480a4b6a66f77dc9c7a
Churikov-SV <Churikov.SV@outlook.com>
2024-09-27 22:49:33 +0300

    Add final structure files

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import controller.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        controller controller = new controller();
+        Controller controller = new Controller();
         controller.start();
     }
 }
